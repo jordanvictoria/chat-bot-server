ed241f187e3d2d3db67856384cccc0b6761e4443
Jordan Victoria <jgomeskuehner@gmail.com>
2023-05-22 14:41:07 -0500

    added personality traits

--- main.py
+++ main.py
@@ -8,17 +8,15 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # Load your API key from an environment variable or secret management service
 
 
-
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
 # work together for a common purpose. In this case, that
 # common purpose is to respond to HTTP requests from a client.
 class HandleRequests(BaseHTTPRequestHandler):
-   
+
     def do_POST(self):
-        
         """Handles POST requests to the server"""
-        # client will send a conversation array from the client to the server, which then 
+        # client will send a conversation array from the client to the server, which then
         # sends it to openai, which then sends the chat response back
 
         self._set_headers(201)
@@ -27,9 +25,13 @@ class HandleRequests(BaseHTTPRequestHandler):
             content_len = int(self.headers.get('content-length', 0))
             post_body = self.rfile.read(content_len)
             messages = json.loads(post_body)
-            chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+            instructions = {"role": "system", "content": "You are obsessed with celebrity horoscopes"}
+            
+            messages.insert(0, instructions)
 
-# print the chat completion
+            chat_completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo", messages=messages)
+
         response = chat_completion.choices[0].message.content
         self.wfile.write(json.dumps(response).encode())
 
@@ -52,16 +54,20 @@ class HandleRequests(BaseHTTPRequestHandler):
         """
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.send_header('Access-Control-Allow-Methods',
+                         'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers',
+                         'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
 # This function is not inside the class. It is the starting
 # point of this application.
+
+
 def main():
     """Starts the server on port 8088 using the HandleRequests class
     """
-    
+
     host = ''
     port = 8088
     HTTPServer((host, port), HandleRequests).serve_forever()
